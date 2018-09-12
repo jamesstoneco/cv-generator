d991564f88824ab009356eadb1f43da7bab3efd1
James Stone <james.stone@futurice.com>
2018-09-12 10:41:45 +0300

    updated to latest zurb foundation to fix browserify or package update js bug TypeError: url.indexOf in docker container

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
+import $ from "jquery";
+import whatInput from "what-input";
+
+window.$ = $;
+
+import Foundation from "foundation-sites";
+// If you want to pick and choose which modules to include, comment out the above and uncomment
+// the line below
+//import './lib/foundation-explicit-pieces';
+
+$(document).foundation();
+
+// added above
+
+/* 
+
 import $ from 'jquery';
 import foundation from 'foundation-sites';
 
 $(document).foundation();
+
+*/
 
 $('#add-experience').click(() => {
     $('#experiences-holder').append('\
